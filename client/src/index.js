37f38b091f05ae076956f18d33e317c4466c7928
aman13e <amanuelaweke13@gmail.com>
2024-05-14 23:59:33 +0300

    update packages

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,6 @@ import thunk from "redux-thunk";
 import { reducers } from "./reducers";
 
 import App from "./App";
-import styles from "./index.css";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
@@ -18,7 +17,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
